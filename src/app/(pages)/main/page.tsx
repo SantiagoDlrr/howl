4e37a828775efc50944a83864f9fc8da695049d1
SantiagoDlrr <A01749140@tec.mx>
2025-03-26 16:22:57 -0600

    mainP

---
+++ src/app/(pages)/main/page.tsx
@@ -0,0 +1,10 @@
+import SignUpCard from "howl/app/_components/auth/signupCard";
+
+export default function mainPage (){
+
+    return(
+        <div className="min-h-[calc(100vh-73px)] flex justify-center items-center">
+            <div>Main Page</div>
+        </div>
+    );
+}
