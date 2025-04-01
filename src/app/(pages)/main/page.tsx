85c3fa7d19f717867c23eb2fecd6adf4e9b72a71
SantiagoDlrr <A01749140@tec.mx>
2025-04-01 12:09:07 -0600

    Added upload file functionality

--- src/app/(pages)/main/page.tsx
+++ src/app/(pages)/main/page.tsx
@@ -2,6 +2,7 @@ import SignUpCard from "howl/app/_components/auth/signupCard";
 import Button from "../../_components/button"
 import CallLog from "howl/app/_components/main_page/callLog";
 import Image from "next/image"
+import DocumentPanel from "howl/app/_components/main_page/panels/documentPanel";
 
 export default function mainPage (){
 
@@ -9,26 +10,10 @@ export default function mainPage (){
         <div className="h-[calc(100vh-73px)] flex justify-center items-stretch pt-20">
 
             
-            <CallLog />
+            <CallLog /> 
 
-             
-            <div className="flex flex-col flex-[5.8] h-full border-black border-opacity-50 border-t border-b-0 border-r-0 border-l border-2">
-
-                <div className="border-black border-opacity-50 border-t-0 border-b border-r-0 border-l-0 border-2 p-3">
-                Análisis de Llamada
-               </div>
-
-               <div className="flex justify-center items-center flex-1">
-                    <div className="flex flex-col items-center border-black border-opacity-50 border-dashed border-2 py-20 px-24">
-                        <Image src="/images/uploadc.png" alt="Microsoft Logo" width={75} height={50} />
-                        <h1 className="text-4xl font-bold text-primary">
-                            Analiza y Transcribe tu archivo
-                        </h1>
-                        <p className="text-2xl py-3">Arrastra o haz click para cargar</p>
-                    </div>
-               </div>
-        
-            </div>
+            
+            <DocumentPanel />
             
 
 
