64489f1e6b114ef33b92bf624f90415c5f4a3769
SantiagoDlrr <A01749140@tec.mx>
2025-04-01 07:05:12 -0600

    main layout

--- src/app/(pages)/main/page.tsx
+++ src/app/(pages)/main/page.tsx
@@ -1,47 +1,18 @@
 import SignUpCard from "howl/app/_components/auth/signupCard";
 import Button from "../../_components/button"
+import CallLog from "howl/app/_components/main_page/callLog";
 import Image from "next/image"
 
 export default function mainPage (){
 
     return(
-        <div className="h-[calc(100vh-73px)] w-[1512px] flex justify-center items-stretch pt-20">
+        <div className="h-[calc(100vh-73px)] flex justify-center items-stretch pt-20">
 
-            <div className= "flex flex-col flex-[2.1]">
-               
-               <div className="border-black border-opacity-50 border-t border-b border-r-0 border-2 p-3">
-                Historial de Llamadas
-               </div>
-
-
-               <div className="flex flex-col justify-between flex-1 p-6 ">
-
-                <div>
-                    <Button label="+ Agregar Nueva Llamada" xl secondary />
-
-
-                    <div className="mt-6 mb-3 font-bold">
-                        Hoy
-                    </div>
-
-                    <div className="bg-purple-400 bg-opacity-30 rounded p-2">
-                        Neoris - venta - ene 16
-                    </div>
-
-                    <div className="p-2 mt-2">
-                        Cliente - Categoria - Fecha 
-                    </div>
-
-                </div>
-
-                <Button label="Guardar llamadas en el Log" xl></Button>
-
-               </div>
-
-            </div>
+            
+            <CallLog />
 
              
-            <div className="flex flex-col flex-[5.8] h-full border-black border-opacity-50 border-t border-b-0 border-r border-l border-2">
+            <div className="flex flex-col flex-[5.8] h-full border-black border-opacity-50 border-t border-b-0 border-r-0 border-l border-2">
 
                 <div className="border-black border-opacity-50 border-t-0 border-b border-r-0 border-l-0 border-2 p-3">
                 Análisis de Llamada
@@ -59,7 +30,10 @@ export default function mainPage (){
         
             </div>
             
-            <div className="border-black border-2 border-opacity-30 flex flex-[2.1]">Ai Asistant</div>
+
+
+
+            <div className="border-black border-2 border-opacity-30 flex flex-[2.1] h-full">Ai Asistant</div>
 
         </div>
     );
