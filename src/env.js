ef030a5c78db0be6e35acac75ede49e673b36d58
Jesus <jadrianlg16@gmail.com>
2025-04-02 08:34:07 -0600

    .

--- src/env.js
+++ src/env.js
@@ -28,7 +28,7 @@ export const env = createEnv({
   /**
    * Specify your client-side environment variables schema here. This way you can ensure the app
    * isn't built with invalid env vars. To expose them to the client, prefix them with
-   * `NEXT_PUBLIC_`.
+   * NEXT_PUBLIC_.
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
