56787b17b10f99b4d878d9edcce3014029c9e4b1
Alejandra Coeto <a01285221@tec.mx>
2025-04-02 10:39:04 -0600

    Update nav bar

--- src/app/utils/routes.ts
+++ src/app/utils/routes.ts
@@ -1,24 +1,18 @@
 export const routes = [
     {
         id: 0,
-        label: "Home",
-        href: "/",
-        isSelected: true
-    },
-    {
-        id: 1,
         label: "Dashboard",
         href: "/dashboard",
         isSelected: false
     },
     {
-        id: 2,
+        id: 1,
         label: "Llamadas",
         href: "/main",
         isSelected: false
     },
     {
-        id: 3,
+        id: 2,
         label: "Logs",
         href: "/logs",
         isSelected: false
