861a878ee50194013e5fd238a33815a995f26905
Mónica Soberón Zubía <monicasoberon2747@gmail.com>
2025-04-01 20:13:00 -0600

    borrar get de error

--- src/app/api/call_analysis/route.ts
+++ src/app/api/call_analysis/route.ts
@@ -29,32 +29,6 @@ export async function GET(request: Request) {
   }
 }
 
-// Función para obtener detalles de una llamada específica
-export async function GET_CALL_DETAILS(request: Request, { params }: { params: { callId: string } }) {
-  try {
-    // Extraer el callId de los parámetros de la URL
-    const { callId } = params;
-
-    if (!callId) {
-      return NextResponse.json({ error: 'Call ID not provided' }, { status: 400 });
-    }
-
-    // Consultar los detalles de la llamada
-    const callDetails = await query(`
-      SELECT c.date, c.feedback, c.main_ideas, e.name as emotion, c.sentiment_analysis,
-      c.output, c.risk_words, c.summary
-      FROM calls c
-      LEFT JOIN call_emotions ce ON ce.call_id = c.id
-      LEFT JOIN emotions e ON e.id = ce.emotion_id
-      WHERE c.id = $1;
-    `, [callId]);
-
-    return NextResponse.json(callDetails);
-  } catch (error) {
-    console.error('Error fetching call details:', error);
-    return NextResponse.error();
-  }
-}
 
 export async function POST(request: Request) {
     try {
