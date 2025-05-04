4ffc9f250090ffe05afc4516ec23ac810b051f97
Mónica Soberón Zubía <monicasoberon2747@gmail.com>
2025-05-04 15:27:01 -0700

    arreglar_cypress_config

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     specPattern: 'tests/e2e-cypress/tests/**/*.cy.{js,ts,jsx,tsx}',
-    baseUrl: "http://localhost:3000",
+    baseUrl: 'https://howl-eight.vercel.app',
     supportFile: 'tests/e2e-cypress/support/e2e.ts',
     setupNodeEvents(on, config) {
       // implement node event listeners here
