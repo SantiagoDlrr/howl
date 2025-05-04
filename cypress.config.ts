d9b9d2c6e7fbcef8354b0cd186eccfe35aefff6c
Mónica Soberón Zubía <monicasoberon2747@gmail.com>
2025-05-04 16:34:22 -0700

    pruebas pasan lcoal pero no ci quiero ver errores

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     specPattern: 'tests/e2e-cypress/tests/**/*.cy.{js,ts,jsx,tsx}',
-    baseUrl: 'https://howl-eight.vercel.app',
+    //baseUrl: 'https://howl-eight.vercel.app',
+    baseUrl: 'http://localhost:3000',
     supportFile: 'tests/e2e-cypress/support/e2e.ts',
     setupNodeEvents(on, config) {
       // implement node event listeners here
