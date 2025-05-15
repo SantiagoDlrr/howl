8cf1b566c64bdd273ebf50e9365d4d31e6962090
Jesus <jadrianlg16@gmail.com>
2025-05-14 23:54:12 -0600

    folder structure + changed main storage from use state to local storage

---
+++ src/app/_components/SCD/DeepAnalisis/models/types.ts
@@ -0,0 +1,33 @@
+// src/DeepAnalisis/models/types.ts
+
+export interface FileData {
+    id: number;
+    name: string;
+    date: string;
+    type: string;
+    duration: string;
+  
+    report: {
+      feedback: string;
+      keyTopics: string[];
+      emotions: string[];
+      sentiment: string;
+      output: string;
+      summary: string;
+      rating: number;
+    };
+  
+    transcript?: {
+      speaker: string;
+      text: string;
+    }[];
+  
+    messages?: {
+      role: "user" | "assistant";
+      text: string;
+    }[];
+  }
+  
+  export interface Response {
+
+  }
