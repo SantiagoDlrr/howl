971e953efaf5669c01d2206ef87e9f199d5df0c1
Jesus <jadrianlg16@gmail.com>
2025-06-04 15:37:48 -0600

    lint fix?

--- src/app/utils/types/html2pdf.d.ts
+++ src/app/utils/types/html2pdf.d.ts
@@ -1,4 +1,41 @@
 declare module 'html2pdf.js' {
-  const html2pdf: any;
+  interface Html2PdfOptions {
+    margin?: number | [number, number] | [number, number, number, number];
+    filename?: string;
+    image?: {
+      type?: string;
+      quality?: number;
+    };
+    html2canvas?: {
+      scale?: number;
+      useCORS?: boolean;
+      letterRendering?: boolean;
+      scrollY?: number;
+      [key: string]: unknown;
+    };
+    jsPDF?: {
+      unit?: string;
+      format?: string;
+      orientation?: string;
+      compress?: boolean;
+      [key: string]: unknown;
+    };
+    pagebreak?: {
+      mode?: string | string[];
+      [key: string]: unknown;
+    };
+  }
+
+  interface Html2PdfInstance {
+    set(options: Html2PdfOptions): Html2PdfInstance;
+    from(element: HTMLElement): Html2PdfInstance;
+    save(): Promise<void>;
+    output(type?: string): Promise<string | Blob>;
+    outputPdf(type?: string): Promise<string | Blob>;
+    then(onFulfilled?: () => void, onRejected?: (error: Error) => void): Promise<void>;
+  }
+
+  function html2pdf(): Html2PdfInstance;
+
   export default html2pdf;
 }
